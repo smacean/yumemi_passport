5ebdc3d6fe891220197f93c160732de0c13b6bf2
smacean <kazukida30@gmail.com>
2025-05-08 19:59:31 +0900

    prittier,eslint設定

--- population-trend/eslint.config.mjs
+++ population-trend/eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
