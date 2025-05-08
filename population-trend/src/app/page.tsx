b2e85c1dc6afe4321572271bf05efa6e1ed7d48a
smacean <kazukida30@gmail.com>
2025-05-08 22:42:48 +0900

    feat: checkboxの実装

--- population-trend/src/app/page.tsx
+++ population-trend/src/app/page.tsx
@@ -2,8 +2,11 @@
 
 import { PopulationAPI, PrefectureAPI } from '@/api/api';
 import { getPopulation, getPrefectures } from '@/api/implement';
-import Image from 'next/image';
 import { useEffect, useState } from 'react';
+
+interface checkBoxStatusProps {
+  [key: number]: boolean;
+}
 
 export default function Home() {
   const [prefectures, setPrefectures] =
@@ -11,145 +14,77 @@ export default function Home() {
   const [populationData, setPopulationData] =
     useState<PopulationAPI['response']['result']>();
 
-  const [selectedPrefCode, setSelectedPrefCode] = useState<number>(1);
+  const [checkBoxStatus, setCheckBoxStatus] = useState<checkBoxStatusProps>(
+    Object.fromEntries(Array.from({ length: 47 }, (_, i) => [i + 1, false])),
+  );
 
   const fetchPrefectures = async () => {
-    console.log('fetchPrefectures');
     const res = await getPrefectures();
     if (res instanceof Error) {
       alert(res.message);
       return;
     }
-    console.log(res);
     setPrefectures(res.result);
-    console.log(prefectures);
   };
 
   const fetchPopulation = async (prefCode: number) => {
-    console.log('fetchPopulation');
     const res = await getPopulation({ prefCode });
     if (res instanceof Error) {
       alert(res.message);
       return;
     }
-    console.log(res);
     setPopulationData(res.result);
-    console.log(populationData);
   };
 
   useEffect(() => {
     fetchPrefectures();
-    fetchPopulation(selectedPrefCode);
   }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="flex flex-col gap-2 items-center justify-center">
+      <div className="flex flex-row items-center justify-center flex-wrap w-xl my-auto">
+        {prefectures?.map((prefecture) => {
+          const prefCode = prefecture.prefCode;
+          const prefName = prefecture.prefName;
+          return (
+            <div key={prefCode} className="gap-2 mx-4 my-1">
+              <label>
+                <input
+                  type="checkbox"
+                  name={prefName}
+                  id={prefCode.toString()}
+                  checked={checkBoxStatus[prefCode]}
+                  onChange={() =>
+                    setCheckBoxStatus((prev) => ({
+                      ...prev,
+                      [prefCode]: !prev[prefCode],
+                    }))
+                  }
+                />
+                {prefName}
+              </label>
+            </div>
+          );
+        })}
+      </div>
+      <div className="flex flex-row gap-2 items-center justify-center flex-wrap">
+        <button onClick={fetchPrefectures} className="outlined">
+          県リロード
+        </button>
         <button
           onClick={() => fetchPopulation(selectedPrefCode)}
           className="outlined"
         >
-          リロード
+          人口リロード
         </button>
-        {prefectures?.forEach((prefecture) => {
-          <p>prefecture.prefName</p>;
+        {prefectures?.map((prefecture) => {
+          const prefCode = prefecture.prefCode;
+          const prefName = prefecture.prefName;
+          if (!checkBoxStatus[prefCode]) return null;
+          return <div key={prefCode}>{prefName}</div>;
         })}
         {populationData?.boundaryYear || ' '}
       </div>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{' '}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
